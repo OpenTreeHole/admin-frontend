2cbd9f01abfd1ca384350c609b2745c651f658ec
Zecyel <zecyel@163.com>
2023-10-20 12:02:26 +0800

    feat: 实现了tag页面的分页机制

--- src/view/tag.r.ts
+++ src/view/tag.r.ts
@@ -3,39 +3,59 @@ import { ListAllTagResponse, listAllTag } from "../api/tag/list";
 // import { toasts } from "../util/dialog";
 
 let tag_list: ListAllTagResponse = []
+let reg = d("")
+let filtered_list: ListAllTagResponse = []
+const page_size = 20;
+let cur_page = 0;
+let max_page = 0;
 
-let update = view((_) => {
+let update_data = view((_) => {
   listAllTag({}).then((data) => {
     tag_list = data
+    filtered_list = tag_list.filter(() => true)
+    update_paging()
     _.$update()
   })
 })
 
-let reg = d("")
+function update_filter() {
+  try {
+    let reg_tester = new RegExp(reg.value)
+    filtered_list = tag_list.filter((tag) => reg_tester.test(tag.name))
+    update_paging()
+  } catch (e) {
+    // ignore it
+    // because reg may be invalid
+  }
+}
 
-function current_tag_list() {
-  let reg_tester = new RegExp(reg.value)
-  return tag_list.filter((tag) => {
-    return reg_tester.test(tag.name)
-  })
+function update_paging() {
+  cur_page = 0;
+  max_page = Math.ceil(filtered_list.length / page_size) - 1;
+}
+
+function paged_list(): ListAllTagResponse {
+  return filtered_list.slice(page_size * cur_page, Math.min(page_size * (cur_page + 1), filtered_list.length))
 }
 
 export default view((_) => {
   if (tag_list.length === 0) {
-    _.embed(update)
+    _.embed(update_data)
   }
   _.$css`margin: 100px;`
   _._div({}, (_) => {
     _.$css`margin-top: 100px; margin-bottom: 20px;`
     _.mdSheet((_) => {
       _.mdTitle("Tag管理页面")
-      _.mdInput(reg, "筛选，支持正则表达式")
+      if (_.mdInput(reg, "筛选，支持正则表达式")) { // oninput event
+        update_filter()
+      }
       if (_.mdButton("刷新列表")) {
-        _.embed(update)
+        _.embed(update_data)
       }
     })
   
-    _.$css`margin-button: 100px; margin-top: 20px;`
+    _.$css`margin-button: 20px; margin-top: 20px;`
     _.mdTable(
       (_) => {
         _._th({}, "#id")
@@ -45,24 +65,48 @@ export default view((_) => {
       },
       (_) => {
         _.for(
-          current_tag_list(),
+          paged_list(),
           (_item, index) => index,
           (value, index) => {
             _._tr({}, (_) => {
               _._td({}, value.id)
               _._td({}, value.name)
               _._td({}, value.temperature)
-              // _.mdDialog("tag迁移", (_) => {
+              _.mdDialog(
+                "tag迁移",
+                (close) => (_) => {
 
-              // }, (_) => {
-              //   _.mdButton
-              // })
-              _.$css`margin: 10px;`
-              _.mdIntrinsicButton("迁移", "primary", false)
+                },
+                (close) => (_) => {
+                  if (_.mdButton("确认")) {
+                    close();
+                    // do something
+                  }
+                  _.mdButton("取消") && close();
+                },
+                (open) => {
+                  _.$css`margin: 10px;`
+                  _.mdIntrinsicButton("迁移", "primary", false) && open();
+                }
+              )
             })
           }
         )
       }
     )
+    _.$css`margin-top: 20px; margin-bottom: 100px; padding: 10px;`
+    _.mdSheet((_) => {
+      _.mdToolbar((_) => {
+        if (_.mdIconButton("arrow_back", false, cur_page === 0)) {
+          cur_page -= 1;
+        }
+        _.mdSpacer()
+        _.mdTitle(`${cur_page * page_size + 1} - ${Math.min((cur_page + 1) * page_size, filtered_list.length)}`)
+        _.mdSpacer()
+        if (_.mdIconButton("arrow_forward", false, cur_page === max_page)) {
+          cur_page += 1;
+        }
+      }, false)
+    })
   })
 })
