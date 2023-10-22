038e9ddab7d4acabe7e4ad806e79e43eb1152e11
Zecyel <zecyel@163.com>
2023-10-22 21:34:17 +0800

    fix: 更新了tsconfig，将server.ts的服务器路径改成从环境获取

--- src/util/server.ts
+++ src/util/server.ts
@@ -1,5 +1,2 @@
-export const AUTH = "https://auth.fduhole.com/api";
-// export const AUTH = "https://auth.jingyijun.xyz:9443/api";
-
-export const TREEHOLE = "https://www.fduhole.com/api";
-// export const TREEHOLE = "https://hole.jingyijun.xyz:9443/api";
+export const AUTH: string = import.meta.env.AUTH
+export const TREEHOLE: string = import.meta.env.TREEHOLE
